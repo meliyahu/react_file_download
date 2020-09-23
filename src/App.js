22998ea4fcb92a368a542d41371ab4f6a71e6117
Mosheh EliYahu <mosheh.eliyahu@adelaide.edu.au>
2020-09-23 17:08:12 +0930

    Feat. check in stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
+import fileDownload from "js-file-download";
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const img_url = "https://swift.rc.nectar.org.au/v1/AUTH_05bca33fce34447ba7033b9305947f11/bioimages/public/phenocam/vicd_whroo/overstorey_oblique_01/2000/200001/1920_vicd_whroo_overstorey_oblique_01_whroo20000101_200045M_tern_phenocam_2000-01-01T000000-1100.jpg"
+  const handleDownload = (url) => {
+    const fileName = img_url.split("/").pop();
+    axios
+      .get(url, {
+        responseType: "blob",
+      })
+      .then((res) => fileDownload(res.data, fileName));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{ width: "200px", height:"50px"}} onClick={() => handleDownload(img_url)}></button>      
     </div>
   );
 }
